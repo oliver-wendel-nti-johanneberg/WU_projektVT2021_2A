0996f4f842e169ecef33e14b062fd34084388ae0
oliver-wendel-nti-johanneberg <74712544+oliver-wendel-nti-johanneberg@users.noreply.github.com>
2021-03-19 15:55:37 +0100

    testat färgpallet, mer CSS fredag v.11

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+const nav_list = document.querySelector('.nav_list')
+const nav_lines = document.querySelector('.nav_lines')
 
-function toogle_burger() {
-    
+function toogleNav() {
+    nav_list.classList.toggle('list_toggle')
+    nav_lines.classList.toggle('burger_toggle')
 }
+
+nav_lines.addEventListener('click', toogleNav)
